e37cbcf7609a9836160a58375b5b5481e1dda853
Ritik Yadav <ritikyadav2325@gmail.com>
2023-11-30 19:00:51 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python 3.9 -m pip install -r requirements.txt
-python 3.9 manage.py collectstatic --noinput --clear
+python 3.10 -m pip install -r requirements.txt
+python 3.10 manage.py collectstatic --noinput --clear
 echo " BUILD START"
